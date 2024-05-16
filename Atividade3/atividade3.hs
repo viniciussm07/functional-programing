fa6f70ea03d6f81b80919edb3565a3bfbd0b1626
viniciussm07 <vini.soares.martins@gmail.com>
2024-05-16 15:52:22 -0300

    Atividade 3 completa

--- Atividade3/atividade3.hs
+++ Atividade3/atividade3.hs
@@ -1,22 +1,27 @@
 main :: IO ()
 main = do
+    l1 <- getLine
+    let x = map read (words l1) :: [Int]
     putStr $
         show $
         length $
-        maiorSegmentoCrescente [1, 2, 3, 2, 3, 4, 1, 2]
-
-maiorSegmentoCrescente :: Ord a => [a] -> [a]
-maiorSegmentoCrescente xs = maior (segmentosCrescentes xs)
+        removeIguais $
+        maiorLista $
+        segmentosCrescentes x
 
 segmentosCrescentes :: Ord a => [a] -> [[a]]
 segmentosCrescentes [] = []
 segmentosCrescentes [x] = [[x]]
 segmentosCrescentes (x:y:xs)
-    | x <= y    = (x : head segs) : tail segs
+    | x < y = (x : head segs) : tail segs
     | otherwise = [x] : segs
     where segs = segmentosCrescentes (y:xs)
 
-maior :: Ord a => [[a]] -> [a]
-maior [] = []
-maior [x] = x
-maior (x:y:xs) = if length x >= length y then maior (x:xs) else maior (y:xs)
+maiorLista :: Ord a => [[a]] -> [a]
+maiorLista [] = []
+maiorLista [x] = x
+maiorLista (x:y:xs) = if length x >= length y then maiorLista (x:xs) else maiorLista (y:xs)
+
+removeIguais :: Eq a => [a] -> [a]
+removeIguais [] = []
+removeIguais (x:xs) = x : removeIguais (filter (/= x) xs)
