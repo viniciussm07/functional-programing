26db75441cc631c7af35b82f5d5b8aae4514468d
viniciussm07 <vini.soares.martins@gmail.com>
2024-06-01 17:43:06 -0300

    Atualizando criaFrames e casos teste

--- Atividade4/src/atividade4.hs
+++ Atividade4/src/atividade4.hs
@@ -2,38 +2,39 @@ main :: IO ()
 main = do
     l1 <- getLine
     let x = map read (words l1) :: [Int]
-    putStr $
-        show $ criaFrames x
+    -- putStr $ show $ criaFrames x
+
+    -- Printa os frames formatados em ordem decrescente 
+    mapM_ (\(i, frame) -> putStrLn $ "Frame " ++ show i ++ " : " ++ show (primeiraBola frame) ++ ", " ++ show (segundaBola frame) ++ ", " ++ show (bolaExtra1 frame) ++ ", " ++ show (bolaExtra2 frame)) (zip [10,9..] (criaFrames x))
 
 -- Estruturas de dados
-data Frame = Frame { primeiraBola :: Int, segundaBola :: Int }
+data Frame = Frame { primeiraBola :: Int, segundaBola :: Int, bolaExtra1 :: Int, bolaExtra2 :: Int}
     deriving (Show, Read)
 
-data Placar = Placar { frames :: [Frame], bonus :: [Int], pontuacao :: Int}
+data Placar = Placar { frames :: [Frame], pontuacao :: Int}
     deriving (Show, Read)
 
--- Funções
 -- Função que recebe uma lista de inteiros e retorna um Placar
 -- com os frames e a pontuação corretas
 -- criaPlacar :: [Int] -> Placar
 -- criaPlacar [] = Placar [] [] 0
 -- criaPlacar (x:xs) = Placar (criaFrames1 (x:xs)) (criaBonus (x:xs)) (calculaPontuacao (criaFrames1 (x:xs)) (criaBonus (x:xs)))
 
--- Função que recebe uma lista de inteiros e retorna uma lista de Frames (sem contar os bônus finais)
-
+-- Função que recebe uma lista de inteiros e retorna uma lista de Frames contando as bolas extras caso existam
 criaFrames :: [Int] -> [Frame]
 criaFrames = go []
     where 
         go frames [] = frames
         go frames (x:xs)
+            -- caso base
             | length frames == 10 = frames
-            | x == 10 = go (Frame x 0 : frames) xs
-            | otherwise = go (Frame x (head xs) : frames) (tail xs)
-
--- Função que recebe uma lista de inteiros e retorna uma lista de inteiros
-criaBonus :: [Int] -> [Int]
-criaBonus [] = []
-criaBonus (x:xs)
-    | x == 10 = head xs : head (tail xs) : criaBonus xs
-    | x + head xs == 10 = head (tail xs) : criaBonus (tail xs)
-    | otherwise = criaBonus (tail xs)
+            -- caso em que a primeira bola do último frame é 10 e a primeira bola extra também é 10
+            | length frames == 9 && x == 10 && (head xs) == 10 = go (Frame x 0 (head xs) (head(tail xs)) : frames) (tail xs)
+            -- caso em que a primeira bola do último frame é 10 e a primeira bola extra não é 10
+            | length frames == 9 && x == 10 = go (Frame x 0 (head xs) 0 : frames) (tail xs)
+            -- caso em que a primeira e segunda bola do último frame somam 10 (spare)
+            | length frames == 9 && x + (head xs) == 10 = go (Frame x (head xs) (head(tail xs)) 0 : frames) (tail xs)
+            -- caso strike básico
+            | x == 10 = go (Frame x 0 0 0 : frames) xs
+            -- caso básico
+            | otherwise = go (Frame x (head xs) 0 0 : frames) (tail xs)
